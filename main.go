48ec14d1ae08ca7a8cffe53658300bcd34a5ff2b
Alex Useche <alex.useche@catapultsystems.com>
2018-04-07 15:01:35 -0500

    can update all repos with new commands now

--- main.go
+++ main.go
@@ -4,26 +4,24 @@ import (
 	"fmt"
 	"os"
 	"flag"
-	"os/signal"
-	"sync"
 	"io/ioutil"
 	"log"
 	"os/exec"
 )
 
 type State struct {
-	Path         string
-	Username     string
-	Password     string
-	NewOrigin    string
-	ChangeOrigin bool
-	Pull         bool
-	Verbose      bool
-	StdIn        bool
-	Quiet        bool
-	Threads      int
-	Terminate	 bool
-	SignalChan   chan os.Signal
+	Path         	string
+	Username     	string
+	Password     	string
+	NewOriginBase   string
+	ChangeOrigin 	bool
+	Pull         	bool
+	Verbose      	bool
+	StdIn        	bool
+	Quiet        	bool
+	Threads      	int
+	Terminate	 	bool
+	SignalChan   	chan os.Signal
 }
 
 func ParseCmdLine() *State {
@@ -34,11 +32,13 @@ func ParseCmdLine() *State {
 	}
 	s.Threads = 10
 	flag.StringVar(&s.Path, "p", "", "Repositories file path")
+	flag.StringVar(&s.NewOriginBase, "o", "", "New origin base file path")
 	flag.StringVar(&s.Username, "U", "", "Username for git Auth")
 	flag.StringVar(&s.Password, "P", "", "Password for git Auth")
 	flag.BoolVar(&s.Pull, "u", false, "Update all repositories in file path (must provide file path)")
 	flag.BoolVar(&s.Verbose, "v", false, "Verbose output")
 	flag.BoolVar(&s.Quiet, "q", false, "Quit output")
+	flag.BoolVar(&s.ChangeOrigin, "c", false, "Change git repo origin")
 	flag.Parse()
 
 	Banner(&s)
@@ -50,6 +50,17 @@ func ParseCmdLine() *State {
 
 	if s.Path[len(s.Path)-1:] != "\\" || s.Path[len(s.Path)-1:] == "/"{
 		s.Path += "\\"
+	}
+
+	if s.ChangeOrigin{
+		if s.NewOriginBase == ""{
+			fmt.Println("[!] You must provide the new base URL for the origin")
+			valid = false
+		} else {
+			if s.NewOriginBase[len(s.NewOriginBase)-1:] != "/"{
+				s.NewOriginBase += "/"
+			}
+		}
 	}
 
 	if valid {
@@ -77,69 +88,69 @@ func Banner(state *State) {
 	Ruler(state)
 }
 
-func PrepareSignalHandler(s *State) {
-	s.SignalChan = make(chan os.Signal, 1)
-	signal.Notify(s.SignalChan, os.Interrupt)
-	go func() {
-		for _ = range s.SignalChan {
-			// caught CTRL+C
-			if !s.Quiet {
-				fmt.Println("[!] Keyboard interrup detected. Terminating...")
-				s.Terminate = true
-				panic("[!] Panic! Panic! Panic!")
-			}
-		}
-	}()
+func Process(s *State) {
 
+	if s.ChangeOrigin{
+		UpdateOrigins(s)
+	}
+
+	if s.Pull{
+		PullAllRepos(s)
+	}
 }
 
-func Process(s *State) {
-	PrepareSignalHandler(s)
-
-	dirChan := make(chan string, s.Threads)
-
-	processorGroup := new(sync.WaitGroup)
-	processorGroup.Add(s.Threads)
-	printerGroup := new(sync.WaitGroup)
-	printerGroup.Add(1)
-
-	// Create goroutines for each thread
-	for i := 0; i < s.Threads; i++ {
-		go func() {
-			for {
-				dir := <-dirChan
-
-				// Are all directories traversed?
-				if dir == "" {
-					break
-				}
-
-				// Mode-specific processing
-				//s.Processor(s, dir)
-			}
-
-			// Indicate to the wait group tha the thread has finished
-			processorGroup.Done()
-		}()
+func PullAllRepos(s *State){
+	folders, err := ioutil.ReadDir(s.Path)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	for _, f := range folders {
+		if f.IsDir(){
+			gitpath := s.Path + f.Name()
+
+			if s.Verbose{
+				fmt.Println("Updating repo " + f.Name())
+				fmt.Println("running command: " + "git -C " + "pull" + gitpath )
+			}
+			out, err := exec.Command("git", "-C", gitpath, "pull").
+				Output()
+			if err != nil{
+				log.Fatal(err)
+			}
+			fmt.Printf("%s\n", out)
+			Ruler(s)
+		}
+	}
+}
+
+func UpdateOrigins(s *State){
 
 	folders, err := ioutil.ReadDir(s.Path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//pwd, _ := os.Getwd()
 	for _, f := range folders {
 		if f.IsDir(){
 			gitpath := s.Path + f.Name()
-			fmt.Println(s.Path + f.Name())
-			out, err := exec.Command("git", "-C", gitpath, "log").
+
+			origin := s.NewOriginBase + f.Name()
+			if s.Verbose{
+				fmt.Println("Updating remote origin for " + f.Name())
+				fmt.Println("running command: " + "git -C " + gitpath + " remote set-url orign")
+			}
+			out, err := exec.Command("git", "-C", gitpath, "remote", "set-url", "origin", origin).
 				Output()
 			if err != nil{
-				fmt.Println(out)
+				log.Fatal(err)
 			}
+			fmt.Printf("%s\n", out)
+			Ruler(s)
 		}
 	}
+
+	Ruler(s)
 }
 
 func main() {
